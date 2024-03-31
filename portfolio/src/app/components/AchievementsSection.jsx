2a9c83844e38deba665373143a9f6ba74e1d7742
B.N.J <benjamin51.simon@gmail.com>
2024-03-31 15:44:39 +0200

    add the animatedNumbers

--- portfolio/src/app/components/AchievementsSection.jsx
+++ portfolio/src/app/components/AchievementsSection.jsx
@@ -7,7 +7,9 @@ import { BestStreak } from '../api/GraphMetrics';
 
 
 const AnimatedNumbers = dynamic(
-  () => import("react-animated-numbers"),
+  () => {
+    return import("react-animated-numbers");
+  },
   { ssr: false }
 );
 
@@ -69,18 +71,34 @@ const AchievementsSection = () => {
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
     <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
       {achievementsList.map((achievement, index) => {
+        const isNumericValue = !isNaN(achievement.value);
         return (
+          
           <div
             key={index}
             className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
           >
             <h2 className="text-white text-4xl font-bold flex flex-row">
               {achievement.prefix}
-              {achievement.metric === "Most Used Language" ? (
-                <span className="text-white text-4xl font-bold">{achievement.value}</span>
-              ) : (
+              {isNumericValue ? 
+              (
+                <AnimatedNumbers
+                key={`animated-${achievement.metric}-${achievement.value}`}
+                includeComma
+                animateToNumber={parseInt(achievement.value, 10)}
+                locale="fr-FR"
+                className="text-white text-4xl font-bold"
+                configs={(_, index) => ({
+                  mass: 1,
+                  friction: 100,
+                  tension: 140 * (index + 1),
+                })}
+                />
+              ) :
+             (
                 <span className="text-white text-4xl font-bold">{achievement.value}</span>
               )}
+             
               {achievement.postfix}
             </h2>
             <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
