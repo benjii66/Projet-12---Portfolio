804670708cb93623d26fdefa10445ac674bb3321
B.N.J <benjamin51.simon@gmail.com>
2024-03-31 14:03:10 +0200

    implement the Hero Section
    Who am I ? Benjamin of course

--- portfolio/src/app/components/HeroSection.jsx
+++ portfolio/src/app/components/HeroSection.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
-  )
-}
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-blue-100">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Benjamin",
+                1000,
+                "Front-End",
+                1000,
+              ]}
+              wrapper="span"
+              speed={20}
+              repeat={Infinity}
+              className="text-2xl sm:text-2xl lg:text-8xl"
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            You'll never walk alone<i>🎶</i>
+          </p>
+          <div>
+            <Link
+              href="/#contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-900 to-blue-300 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              href="/"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-900 to-blue-300 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+        <div className="bg-[#46454559] lg:w-[400px] lg:h-[400px] w-[250px] h-[250px] rounded-full relative mt-20">
+          <Image
+            src="/images/benji.png"
+            alt="hero image"
+            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 lg:top-1/2"
+            width={300}
+            height={300}
+          />
+        </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- portfolio/src/app/components/MenuOverlay.jsx
+++ portfolio/src/app/components/MenuOverlay.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import NavLink from './NavLink'
 
-const MenuOverlay = () => {
+const MenuOverlay = ({links}) => {
   return (
-    <div>MenuOverlay</div>
+    <ul className='flex flex-col py-4 items-center'>
+        {links.map((link, index) => (
+            <li key={index}>
+            <NavLink href={link.path} title={link.title} />
+            </li>
+        ))}
+    </ul>
   )
 }
 
