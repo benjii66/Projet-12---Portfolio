bb6389c59727f31d410d879d7556c05b24d05350
benjii66 <benjamin51.simon@gmail.com>
2024-04-15 11:19:32 +0200

    fix the anchors navigation

--- portfolio/src/app/components/navigation/NavLink.jsx
+++ portfolio/src/app/components/navigation/NavLink.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import Link from"next/link"
+import React from 'react';
+import Link from 'next/link';
 
-const NavLink = ({href, title}) => {
+const NavLink = ({ href, title }) => {
+
+  const handleAnchorClick = (e) => {
+    const hashIndex = href.indexOf('#');
+    if (hashIndex !== -1 && href.slice(0, hashIndex) === window.location.pathname) {
+      e.preventDefault(); 
+      const hash = href.slice(hashIndex + 1);
+      const element = document.getElementById(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }
+  };
+
   return (
-    <Link 
-        href ={href}
-        className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white'
-        aria-label={title}
-        itemScope
-        itemProp={title}
-    >
-        {title}
-    </Link>
-  )
-}
+    <Link
+      href={href}
+      passHref
+      onClick={handleAnchorClick}
+      className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white'
+      aria-label={title}
+      itemScope
+      itemProp={title}>
 
-export default NavLink
+      {title}
+
+    </Link>
+  );
+};
+
+export default NavLink;
